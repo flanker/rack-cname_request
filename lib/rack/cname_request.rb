f9bc7ea7b31f2202afac67ca21e3575f1ca0a1f8
FENG Zhichao <flankerfc@gmail.com>
2018-01-04 13:51:24 +0800

    Location host white list support.
    * When passing a `only` parameter, only redirection location host
    matches will be modified.

--- lib/rack/cname_request.rb
+++ lib/rack/cname_request.rb
@@ -3,26 +3,55 @@ require 'rack/cname_request/version'
 module Rack
   class CnameRequest
 
-    attr_reader :http_cname_header_key
+    attr_reader :http_cname_header_key, :host_whitelist
 
-    def initialize(app, cname_header_key: nil)
+    def initialize(app, cname_header_key: nil, only: [])
       @app = app
       @http_cname_header_key = "HTTP_#{cname_header_key}"
+      @host_whitelist = only || []
     end
 
     def call(env)
       status, headers, body = @app.call env
-
-      if headers['Location'] && env[http_cname_header_key]
-        uri = URI(headers['Location'])
-        uri.host = env[http_cname_header_key]
-        headers['Location'] = uri.to_s
+      original_location = headers['Location']
+      if original_location
+        modifier = LocationModifier.new(original_location, env[http_cname_header_key], host_whitelist: host_whitelist)
+        headers['Location'] = modifier.modified_location if modifier.should_modify?
       end
 
       [status, headers, body]
     end
 
+  end
+
+  class LocationModifier
+
+    attr_reader :location_uri, :cname_header_value, :host_whitelist
+
+    def initialize(location, cname_header_value, host_whitelist: [])
+      @location_uri = URI(location) if location
+      @cname_header_value = cname_header_value
+      @host_whitelist = host_whitelist || []
+    end
+
+    def should_modify?
+       comes_from_cname_proxy? && location_host_in_white_list?
+    end
+
+    def modified_location
+      location_uri.host = cname_header_value
+      location_uri.to_s
+    end
+
     private
+
+    def comes_from_cname_proxy?
+      cname_header_value
+    end
+
+    def location_host_in_white_list?
+      host_whitelist.include?(location_uri.host)
+    end
 
   end
 end
--- spec/rack/cname_request_spec.rb
+++ spec/rack/cname_request_spec.rb
@@ -6,15 +6,25 @@ RSpec.describe Rack::CnameRequest do
 
   def load_app(name)
     Rack::Builder.new do
-      use Rack::CnameRequest, cname_header_key: 'CNAME_FROM'
+      use Rack::CnameRequest, cname_header_key: 'CNAME_FROM', only: 'cname.server.com'
       map('/ok') do
         run proc { |env|
           [200, {'Content-Type' => 'text/html'}, ['some text']]
         }
       end
-      map('/home') do
+      map('/redirect_with_path_only') do
         run proc { |env|
-          [301, {'Content-Type' => 'text/html', 'Location' => 'http://my.test.com/login'}, ['redirecting']]
+          [301, {'Content-Type' => 'text/html', 'Location' => '/path'}, ['redirecting']]
+        }
+      end
+      map('/redirect_with_path_and_different_host') do
+        run proc { |env|
+          [301, {'Content-Type' => 'text/html', 'Location' => 'http://a.new.host.com/path'}, ['redirecting']]
+        }
+      end
+      map('/redirect_with_path_and_original_host') do
+        run proc { |env|
+          [301, {'Content-Type' => 'text/html', 'Location' => 'http://cname.server.com/path'}, ['redirecting']]
         }
       end
     end
@@ -27,15 +37,21 @@ RSpec.describe Rack::CnameRequest do
     expect(last_response.headers['Location']).to be_nil
   end
 
-  it 'does not modify redirection locaiton if request not sent from cname proxy' do
-    get '/home'
-    expect(last_response.headers['Location']).to eq('http://my.test.com/login')
+  it 'does not modify redirection location if request not sent from cname proxy' do
+    get '/redirect_with_path_and_original_host'
+    expect(last_response.headers['Location']).to eq('http://cname.server.com/path')
   end
 
-  it 'modifies redirection location if request sent from cname proxy' do
+  it 'does not modify redirection location if location is not in the white list' do
     header 'CNAME_FROM', 'xyz.custom.com'
-    get '/home'
-    expect(last_response.headers['Location']).to eq('http://xyz.custom.com/login')
+    get '/redirect_with_path_and_different_host'
+    expect(last_response.headers['Location']).to eq('http://a.new.host.com/path')
+  end
+
+  it 'modifies redirection location if request sent from cname proxy and matches white list' do
+    header 'CNAME_FROM', 'xyz.custom.com'
+    get '/redirect_with_path_and_original_host'
+    expect(last_response.headers['Location']).to eq('http://xyz.custom.com/path')
   end
 
 end
